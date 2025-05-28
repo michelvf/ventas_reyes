a53090edf193f79e9de12bb0f480070cd9c7adae
Michel Ernesto Vega Fuenzalida <michel.vega.f@gmail.com>
2025-05-27 21:49:08 -0400

    Para guardar los cambios en la factura_form.html

--- compras/forms.py
+++ compras/forms.py
@@ -197,7 +197,20 @@ class ProductoForm(forms.ModelForm):
         }
 
 
-
+class FacturaForm(forms.ModelForm):
+    """
+    Forumario para la Factura
+    """
+    class Meta:
+        model = Factura
+        fields = ['cliente', 'observaciones', 'bonificacion', 'tipo']
+        widgets = {
+            'cliente': forms.Select(attrs={'class': 'form-control'}),
+            'tipo': forms.Select(attrs={'class': 'form-control'}),
+            'observaciones': forms.Textarea(attrs={'class': 'form-control', 'rows': 1}),
+            'bonificacion': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+        
         # self.fields['subtotal'].widget.attrs['readonly'] = True  # Solo lectura
         # self.fields['total'].widget.attrs['readonly'] = True  # Solo lectura
 
